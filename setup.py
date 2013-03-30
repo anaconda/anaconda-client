d502ebb640eeb14699662c78feee3ebdec3fa744
srossross <srossross@gmail.com>
2013-03-30 10:20:03 -0700

    ENH: initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+'''
+@author: sean
+'''
+
+from setuptools import setup, find_packages
+
+setup(
+    name='binstar_client',
+    version="0.0.1",
+    author='Sean Ross-Ross',
+    author_email='srossross@gmail.com',
+    url='http://github.com/Binstar/binstar_client',
+    packages=find_packages()
+)
+
