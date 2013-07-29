300841f8678e6ab3e765c2ee17886aa2085a6015
srossross <srossross@gmail.com>
2013-07-29 13:29:04 -0700

    ENH: added `auth` sub-command
    User can now have access to the authentication tokens

---
+++ binstar_client/commands/authorizations.py
@@ -0,0 +1,62 @@
+'''
+Manage Authentication tokens
+'''
+from binstar_client.utils import get_binstar
+import getpass
+from dateutil.parser import parse as parse_date
+
+
+def show_auths(authentications):
+    header = {'id': 'ID', 'application': 'Application',
+              'remote_addr':'Remote Addr',
+              'hostname':'Host',
+              'resource':'Resource',
+              'expires':'Expires'}
+    
+    template = '%(id)-25s | %(application)-20s | %(remote_addr)-20s | %(hostname)-20s | %(resource)-20s | %(expires)-20s'
+    print
+    print template % header
+    print '%s-+-%s-+-%s-+-%s-+-%s-+-%s' % ('-' * 25, '-' * 20, '-' * 20, '-' * 20, '-' * 20, '-' * 20)
+    
+    for auth in authentications:
+        if auth['expires']:
+            auth['expires'] = parse_date(auth['expires']).ctime()
+        print template % auth
+
+
+def main(args):
+    binstar = get_binstar()
+    if args.list:
+        show_auths(binstar.authentications())
+    if args.remove:
+        for auth_id in args.remove:
+            binstar.remove_authentication(auth_id)
+            
+    if args.create:
+        
+        username = raw_input('Username: ')
+        password = getpass.getpass()
+        
+        print binstar.authenticate(username, password, 
+                                   application='Binstar Cli', application_url='', 
+                                   scopes=['read','write'], resource=args.resource, max_age=args.max_age)
+        
+    
+    
+def add_parser(subparsers):
+    
+    parser = subparsers.add_parser('auth',
+                                    help='Manage Authorization Tokens',
+                                    description=__doc__)
+    parser.add_argument('-n','--name')
+    parser.add_argument('--max-age', type=int)
+    parser.add_argument('--resource', default='api:**')
+    
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-l', '--list', action='store_true')
+    group.add_argument('-r', '--remove', metavar='ID', nargs='+')
+    group.add_argument('-c', '--create', action='store_true')
+    parser.set_defaults(main=main)
+    
+
+
--- binstar_client/commands/whoami.py
+++ binstar_client/commands/whoami.py
@@ -3,6 +3,7 @@ Print the information of the current user
 '''
 from binstar_client import Unauthorized
 from binstar_client.utils import get_binstar
+from dateutil.parser import parse as parse_date
 
 def main(args):
     binstar = get_binstar()
@@ -12,8 +13,11 @@ def main(args):
     except Unauthorized:
         return 'Anonymous User'
     
+    print 'Username:', user.pop('login') 
+    print 'Member since:', parse_date(user.pop('created_at')).ctime()
+     
     for key_value in user.items():
-        print  '%s: %s' % key_value
+        print  '  +%s: %s' % key_value
 
 def add_parser(subparsers):
     subparser = subparsers.add_parser('whoami', 
