c49f40cc0899cd11b2df1aa3860a4e9d3705d2ff
srossross <srossross@gmail.com>
2013-08-01 07:15:16 -0700

    ENH: updated added mixin to class

--- binstar_client/utils/__init__.py
+++ binstar_client/utils/__init__.py
@@ -16,6 +16,15 @@ import yaml
 import sys
 
 from ..errors import UserError
+import json
+
+
+
+def jencode(payload):
+    return base64.b64encode(json.dumps(payload))
+
+def pv(version):
+    return tuple(int(x) for x in version.split('.'))
 
 class PackageSpec(object):
     def __init__(self, user, package, version, basename, attrs, spec_str):
