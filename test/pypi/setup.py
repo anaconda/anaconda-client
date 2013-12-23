9146c2135ee39acb3cdd420dc7eb5c878869a349
srossross <srossross@gmail.com>
2013-12-23 10:15:44 -0800

    added pypi test package

---
+++ test/pypi/setup.py
@@ -0,0 +1,18 @@
+'''
+@author: sean
+'''
+
+from setuptools import setup, find_packages
+
+setup(
+    name='test_package34',
+    version="0.3",
+    author='Sean Ross-Ross',
+    author_email='srossross@gmail.com',
+    url='http://github.com/binstar/binstar_pypi',
+    packages=find_packages(),
+    description='Python Distribution Utilities',
+    long_description='longer description of the package',
+)
+
+
