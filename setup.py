f4766f570914da272eda1f72fc6c39471d63886c
srossross <srossross@gmail.com>
2014-01-06 16:59:00 -0800

    bump version to 0.4.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='binstar',
-    version="0.4.0",
+    version="0.4.1",
     author='Sean Ross-Ross',
     author_email='srossross@gmail.com',
     url='http://github.com/Binstar/binstar_client',
