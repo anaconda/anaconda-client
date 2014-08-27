9784a6832b53fa545d8e69ccfb5ecc6e925e0154
srossross <srossross@gmail.com>
2014-08-27 13:47:47 -0700

    ENH: added syslog wrappers

--- binstar_client/utils/handlers.py
+++ binstar_client/utils/handlers.py
@@ -1,6 +1,9 @@
 
 import sys
 import logging
+import traceback
+import json
+import os
 
 class MyStreamHandler(logging.Handler):
     WARNING = '\033[93m'
@@ -15,7 +18,7 @@ class MyStreamHandler(logging.Handler):
                  }
 
     def color_map(self, header, level):
-        return self.COLOR_MAP.get(level,'[%s]') % header
+        return self.COLOR_MAP.get(level, '[%s]') % header
 
     def emit(self, record):
         if record.levelno == logging.INFO:
@@ -42,3 +45,58 @@ class MyStreamHandler(logging.Handler):
         else:
             stream.write('%s\n' % message)
 
+class JSONFormatter(object):
+
+    def __init__(self, *args, **extra_tags):
+        self.dumps = extra_tags.pop('dumps', lambda obj: json.dumps(obj, default=lambda obj: str(obj)))
+
+        object.__init__(self, *args)
+        self.extra_tags = extra_tags
+
+    def format(self, record):
+
+        if isinstance(record.msg, dict):
+            data = record.msg
+        elif isinstance(record.msg, (list, tuple)):
+            data = {'items': record.msg}
+        else:
+            data = {'msg':record.msg}
+
+        kwargs = self.extra_tags.copy()
+
+        data.update(logLevel=record.levelname,
+                    logModule=record.module,
+                    logName=record.name,
+                    pid=os.getpid(),
+                    **kwargs)
+
+        if record.exc_info:
+            etype, value, tb = record.exc_info
+            tb = '\n'.join(traceback.format_exception(etype, value, tb))
+            data['exception'] = True
+            data['traceback'] = tb
+
+        msg = self.dumps(data)
+        return msg
+
+class JSONSysLogFormatter(JSONFormatter):
+    def __init__(self, appName, *args, **extra_tags):
+        self.appName = appName
+        JSONFormatter.__init__(self, *args, **extra_tags)
+
+    def format(self, record):
+        msg = JSONFormatter.format(self, record)
+        return '%s %s' % (self.appName, msg)
+
+
+def syslog_handler(app_name='binstar-client'):
+    kw = {}
+    if os.path.exists('/dev/log'):
+        kw['address'] = '/dev/log'
+    elif os.path.exists('/var/run/syslog'):
+        kw['address'] = '/var/run/syslog'
+
+    hdlr = logging.handlers.SysLogHandler(**kw)
+    hdlr.setLevel(logging.INFO)
+    hdlr.setFormatter(JSONSysLogFormatter(app_name))
+    return hdlr
