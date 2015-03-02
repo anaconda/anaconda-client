b8a5bbe3092b74398d2331a7a3c55d5c36bf4e76
malev <marcosvanetta@gmail.com>
2015-03-02 09:26:53 -0600

    Inspect ipython notebook files #127

--- binstar_client/inspect_package/ipynb.py
+++ binstar_client/inspect_package/ipynb.py
@@ -1,15 +1,34 @@
+import json
+
+
+class IPythonNotebook(object):
+    def __init__(self, fileobj):
+        content = json.loads(fileobj.read())
+        self.basename = fileobj.name
+
+        self.signature = content['metadata']['signature']
+        if content['metadata']['name'] == '':
+            self.name = fileobj.name
+        else:
+            self.name = content['metadata']['name']
+
+
 def inspect_ipynb_package(filename, fileobj):
+    ipython_notebook = IPythonNotebook(fileobj)
+
     package_data = {
-        'name': filename,
-        'summary': 'summary',
+        'name': ipython_notebook.name,
+        'summary': '',
     }
     release_data = {
-        'version': '0.0.1',
+        'version': '',
         'description': ''
     }
     file_data = {
         'basename': '',
-        'attrs': {}
+        'attrs': {
+            'signature': ipython_notebook.signature
+        }
     }
 
     return package_data, release_data, file_data
