35a70bef48611f2be09197ae7cbaf056f1169286
malev <marcosvanetta@gmail.com>
2015-03-26 14:44:45 -0500

    Support nbformat 4.0 (ipython 3.0) #135

--- binstar_client/tests/test_ipynb.py
+++ binstar_client/tests/test_ipynb.py
@@ -10,8 +10,29 @@ from binstar_client.inspect_package.ipynb import inspect_ipynb_package
 
 
 class Test(CLITestCase):
-    def test_inspect_ipynb_package(self):
-        with open(self.data_dir('test.ipynb')) as fileobj:
+    def test_inspect_ipynb_3_package(self):
+        with open(self.data_dir('test-3.0.ipynb')) as fileobj:
+            package_data, release_data, file_data = inspect_ipynb_package('test.ipynb', fileobj)
+
+        self.assertEqual({
+            'name': 'test',
+            'summary': 'IPython notebook'
+        },package_data)
+
+        self.assertEqual({
+            'version': '1.0',
+            'description': ''
+        }, release_data)
+
+        self.assertEqual({
+            'basename': 'test.ipynb',
+            'attrs': {
+                'signature': ''
+            }
+        }, file_data)
+
+    def test_inspect_ipynb_2_package(self):
+        with open(self.data_dir('test-2.0.ipynb')) as fileobj:
             package_data, release_data, file_data = inspect_ipynb_package('test.ipynb', fileobj)
 
         self.assertEqual({
