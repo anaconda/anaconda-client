df8976b97eca8e3c4d6ec42e1aed02b4429a339b
malev <marcosvanetta@gmail.com>
2015-05-12 15:50:33 -0500

    Remove Finder from __init__

--- binstar_client/utils/notebook/__init__.py
+++ binstar_client/utils/notebook/__init__.py
@@ -1,5 +1,6 @@
 from .uploader import *
 from .finder import *
+from .scm import *
 
 
 def parse(handle):
@@ -30,60 +31,3 @@ def parse(handle):
         project = notebook
 
     return project, notebook
-
-
-class Finder(object):
-    """
-    You may pass a list of files or a single directory
-    It will return two list:
-    * valid uploadable files
-    * invalid files/directories
-    """
-    extensions = ['ipynb', 'csv', 'json']
-
-    def __init__(self, elements):
-        self.elements = elements
-        self.first = elements[0]
-
-    def parse(self):
-        if self.one_directory():
-            return self.valid_files_from_dir()
-        else:
-            return self.valid_files_from_elements()
-
-    def one_directory(self):
-        return len(self.elements) == 1 and os.path.isdir(self.first)
-
-    def valid_files_from_dir(self):
-        valid = []
-        invalid = []
-        for element in os.listdir(self.elements[0]):
-            if element.startswith('.'):
-                continue
-            elif self.is_valid(element):
-                valid.append(element)
-            else:
-                invalid.append(element)
-
-        return valid, invalid
-
-    def valid_files_from_elements(self):
-        valid = []
-        invalid = []
-
-        for element in self.elements:
-            if self.is_valid(element):
-                valid.append(element)
-            else:
-                invalid.append(element)
-
-        return valid, invalid
-
-    def is_valid(self, element, prefix=''):
-        return os.path.isfile(os.path.join(prefix, element)) and self.valid_extension(element)
-
-    def valid_extension(self, element):
-        for extension in self.extensions:
-            if element.endswith(extension):
-                return True
-        return False
