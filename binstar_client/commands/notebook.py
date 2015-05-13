b4369dbbaeaca2723f225083d05a4fc2bc6c2642
malev <marcosvanetta@gmail.com>
2015-05-13 17:13:23 -0500

    Reorganice cmd notebook

--- binstar_client/commands/notebook.py
+++ binstar_client/commands/notebook.py
@@ -1,21 +1,18 @@
 """
 Usage:
     binstar notebook upload project notebook.ipynb
-    binstar notebook upload project directory/
-
-Deprecated:
-    binstar notebook upload notebook.ipynb
-    binstar notebook upload project:PATH/TO/notebook.ipynb
+    binstar notebook upload project directory
     binstar notebook download project
     binstar notebook download project:notebook
 """
 
 from __future__ import unicode_literals
+import os
 import argparse
 import logging
 from binstar_client import errors
 from binstar_client.utils import get_binstar
-from binstar_client.utils.notebook import Finder, Uploader
+from binstar_client.utils.notebook import Finder, Uploader, SCM, local_files
 
 log = logging.getLogger("binstar.notebook")
 
@@ -51,7 +48,7 @@ def add_parser(subparsers):
 
     parser.add_argument(
         'files',
-        help='Files to puload',
+        help='Files to upload',
         action='append',
         default=[]
     )
@@ -60,26 +57,23 @@ def add_parser(subparsers):
 
 
 def main(args):
-    """
-    1) test if filename exist
-    2) Detect project name or generate it from the notebook
-    3) generate version with timestamp
-    4) Extract summary from notebook file
-    """
-
+    binstar = get_binstar(args)
     if args.action == 'upload':
-        binstar = get_binstar(args)
         finder = Finder(args.files)
-        valid, invalid = finder.parse()
+        uploader = Uploader(binstar, args.project)
+        scm = SCM(uploader, args.project)
+        scm.local(local_files(finder.valid))
+        scm.pull()
 
-        uploader = Uploader(binstar, args.project, username=args.user, version=args.version, summary=args.summary)
-        for filename in valid:
-            if uploader.upload(filename, force=False):
-                log.info("{} has been uploaded.".format(filename))
-            else:
-                raise errors.BinstarError(uploader.msg)
-        for filename in invalid:
-            log.info("{} can't be uploaded.".format(filename))
+        if len(scm.diff) == 0:
+            log.info("There are no files to upload")
+        else:
+            for scm_file in scm.diff:
+                filename = scm_file.filename
+                if uploader.upload(os.path.join(finder.prefix, filename), force=False):
+                    log.info("{} has been uploaded.".format(filename))
+        for f in finder.invalid:
+            log.info("{} can't be uploaded".format(f))
 
     elif args.action == 'download':
         raise errors.BinstarError("Download notebooks hasn't been implemented yet. Soon!")
