14efcc349a3b524345808eaf925399bede34c7c6
malev <marcosvanetta@gmail.com>
2015-05-12 23:36:18 -0500

    make file pep8 compliant

--- binstar_client/errors.py
+++ binstar_client/errors.py
@@ -1,29 +1,33 @@
 from clyent.errors import ClyentError
 
-class BinstarError(ClyentError):
 
+class BinstarError(ClyentError):
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
 
         if not hasattr(self, 'message'):
             self.message = args[0] if args else None
 
+
 class Unauthorized(BinstarError):
     pass
+
 
 class Conflict(BinstarError):
     pass
 
-class NotFound(BinstarError, IndexError):
 
+class NotFound(BinstarError, IndexError):
     def __init__(self, *args, **kwargs):
         BinstarError.__init__(self, *args, **kwargs)
         IndexError.__init__(self, *args, **kwargs)
 
         self.message = args[0]
 
+
 class UserError(BinstarError):
     pass
+
 
 class ServerError(BinstarError):
     pass
