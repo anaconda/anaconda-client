100881d858d60ebf5ed4f4d1543e591da8f33ab9
malev <marcosvanetta@gmail.com>
2015-07-15 12:30:34 -0500

    Replace assert_called_once with called

--- binstar_client/tests/test_login.py
+++ binstar_client/tests/test_login.py
@@ -10,8 +10,8 @@ from binstar_client.tests.urlmock import urlpatch
 import unittest
 from mock import patch
 
+
 class Test(CLITestCase):
-    
     @patch('binstar_client.commands.login.store_token')
     @patch('getpass.getpass')
     @patch('binstar_client.commands.login.input')
@@ -19,19 +19,19 @@ class Test(CLITestCase):
     def test_login(self, urls, input, getpass, store_token):
         input.return_value = 'test_user'
         getpass.return_value = 'password'
-        
+
         auth = urls.register(method='POST', path='/authentications', content='{"token": "a-token"}')
         main(['--show-traceback', 'login'], False)
-        self.assertIn('login successful', self.stream.getvalue()) 
-        
+        self.assertIn('login successful', self.stream.getvalue())
+
         auth.assertCalled()
-        
+
         self.assertIn('Authorization', auth.req.headers)
         self.assertIn('Basic ', auth.req.headers['Authorization'])
-        
-        store_token.assert_called_once()
+
+        self.assertTrue(store_token.called)
         self.assertEqual(store_token.call_args[0][0], 'a-token')
-        
-        
+
+
 if __name__ == '__main__':
     unittest.main()
