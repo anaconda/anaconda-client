9083401b237505aa3bb36e32f6b17443bdb9ca34
Derek Ludwig <dludwig@continuum.io>
2015-11-10 14:17:06 -0800

    Accept args, kwargs on pypi detect code
    Fixes #230

--- binstar_client/inspect_package/pypi.py
+++ binstar_client/inspect_package/pypi.py
@@ -188,7 +188,7 @@ def format_wheel_json_metadata(data, filename, zipfile):
     return package_data, release_data, file_data
 
 
-def inspect_pypi_package_whl(filename, fileobj, *args, **kwargs):
+def inspect_pypi_package_whl(filename, fileobj):
     tf = zipfile.ZipFile(fileobj)
     data = extract_first(tf, '*.dist-info/metadata.json')
     if data is None:
@@ -371,7 +371,7 @@ def inspect_pypi_package_rpm(filename, fileobj):
     return package_data, release_data, file_data
 
 
-def inspect_pypi_package(filename, fileobj):
+def inspect_pypi_package(filename, fileobj, *args, **kwargs):
 
     if filename.endswith('.tar.gz') or filename.endswith('.tar.bz2'):
         return inspect_pypi_package_sdist(filename, fileobj)
