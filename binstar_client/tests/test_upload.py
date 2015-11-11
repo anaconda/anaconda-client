2d39b3c35872d8a811a71ef3de9d549bc6a87af2
Derek Ludwig <dludwig@continuum.io>
2015-11-10 16:16:20 -0800

    Add sdist upload test case

--- binstar_client/tests/test_upload.py
+++ binstar_client/tests/test_upload.py
@@ -44,7 +44,7 @@ class Test(CLITestCase):
         registry.assertAllCalled()
 
     @urlpatch
-    def test_upload(self, registry):
+    def test_upload_conda(self, registry):
 
         registry.register(method='GET', path='/user', content='{"login": "eggs"}')
         registry.register(method='GET', path='/package/eggs/foo', content='{}')
@@ -58,6 +58,24 @@ class Test(CLITestCase):
         registry.register(method='POST', path='/commit/eggs/foo/0.1/osx-64/foo-0.1-0.tar.bz2', status=200, content={})
 
         main(['--show-traceback', 'upload', self.data_dir('foo-0.1-0.tar.bz2')], False)
+
+        registry.assertAllCalled()
+
+    @urlpatch
+    def test_upload_pypi(self, registry):
+
+        registry.register(method='GET', path='/user', content='{"login": "eggs"}')
+        registry.register(method='GET', path='/package/eggs/test-package34', content='{}')
+        registry.register(method='GET', path='/release/eggs/test-package34/0.3.1', content='{}')
+        registry.register(method='GET', path='/dist/eggs/test-package34/0.3.1/test_package34-0.3.1.tar.gz', status=404, content='{}')
+
+        content = {"post_url": "http://s3_url.com/s3_url", "form_data": {}, "dist_id": "dist_id"}
+        registry.register(method='POST', path='/stage/eggs/test-package34/0.3.1/test_package34-0.3.1.tar.gz', content=content)
+
+        registry.register(method='POST', path='/s3_url', status=201)
+        registry.register(method='POST', path='/commit/eggs/test-package34/0.3.1/test_package34-0.3.1.tar.gz', status=200, content={})
+
+        main(['--show-traceback', 'upload', self.data_dir('test_package34-0.3.1.tar.gz')], False)
 
         registry.assertAllCalled()
 
