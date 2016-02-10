60c83f39d7826b3aebc3a599c66e3092a3cae076
Derek Ludwig <dludwig@continuum.io>
2016-02-10 14:35:18 -0800

    Add kerberos login unit-tests

--- binstar_client/tests/test_login.py
+++ binstar_client/tests/test_login.py
@@ -20,6 +20,8 @@ class Test(CLITestCase):
         input.return_value = 'test_user'
         getpass.return_value = 'password'
 
+        urls.register(path='/authentication-type', content='{"authentication_type": "password"}')
+
         auth = urls.register(method='POST', path='/authentications', content='{"token": "a-token"}')
         main(['--show-traceback', 'login'], False)
         self.assertIn('login successful', self.stream.getvalue())
@@ -28,6 +30,51 @@ class Test(CLITestCase):
 
         self.assertIn('Authorization', auth.req.headers)
         self.assertIn('Basic ', auth.req.headers['Authorization'])
+
+        self.assertTrue(store_token.called)
+        self.assertEqual(store_token.call_args[0][0], 'a-token')
+
+    @patch('binstar_client.commands.login.store_token')
+    @patch('getpass.getpass')
+    @patch('binstar_client.commands.login.input')
+    @urlpatch
+    def test_login_compatible(self, urls, input, getpass, store_token):
+        input.return_value = 'test_user'
+        getpass.return_value = 'password'
+
+        urls.register(path='/authentication-type', status=404)
+
+        auth = urls.register(method='POST', path='/authentications', content='{"token": "a-token"}')
+        main(['--show-traceback', 'login'], False)
+        self.assertIn('login successful', self.stream.getvalue())
+
+        auth.assertCalled()
+
+        self.assertIn('Authorization', auth.req.headers)
+        self.assertIn('Basic ', auth.req.headers['Authorization'])
+
+        self.assertTrue(store_token.called)
+        self.assertEqual(store_token.call_args[0][0], 'a-token')
+
+    @patch('binstar_client.commands.login.store_token')
+    @urlpatch
+    def test_login_kerberos(self, urls, store_token):
+
+        urls.register(path='/authentication-type', content='{"authentication_type": "kerberos"}')
+
+        def update_authentications():
+            urls.unregister(auth_fail)
+            auth_success = urls.register(
+                method='POST',
+                path='/authentications',
+                content='{"token": "a-token"}',
+                headers={'WWW-Authenticate': 'Negotiate STOKEN'},
+            )
+
+        auth_fail = urls.register(method='POST', path='/authentications', status=401, headers={'WWW-Authenticate': 'Negotiate'}, side_effect=update_authentications)
+
+        main(['--show-traceback', 'login'], False)
+        self.assertIn('login successful', self.stream.getvalue())
 
         self.assertTrue(store_token.called)
         self.assertEqual(store_token.call_args[0][0], 'a-token')
