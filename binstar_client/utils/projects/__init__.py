f4fbf7b55e974ad236c1f12fc00e85470225a45c
Marcos Vanetta <marcosvanetta@gmail.com>
2016-04-05 12:18:27 -0500

    Refactor organization

--- binstar_client/utils/projects/__init__.py
+++ binstar_client/utils/projects/__init__.py
@@ -1,118 +1,13 @@
-import inspect
 import logging
 import os
 from os import path
 from binstar_client.utils import get_server_api
-from binstar_client.errors import BinstarError
-from .filer import tempfile_tar
 from .filters import filters
 from .inspectors import inspectors
 from .uploader import ProjectUploader
+from .models import CondaProject, PFile
 
 log = logging.getLogger('binstar.projects.upload')
-
-
-class CondaProject(object):
-    # TODO: This class will be moved into Anaconda-Project
-    def __init__(self, project_path, *args, **kwargs):
-        self.project_path = project_path
-        self._name = None
-        self.pfiles = []
-        self.metadata = {
-            'summary': kwargs.get('summary', None),
-            'description': kwargs.get('description', None),
-            'version': kwargs.get('version', None)
-        }
-        self.metadata = dict((k, v) for k, v in self.metadata.items() if v)
-
-    def to_project_creation(self):
-        return {
-            'name': self.name,
-            'access': 'public',
-            'profile': {
-                'description': self.metadata.get('description', ''),
-                'summary': self.metadata.get('summary', ''),
-            }
-        }
-
-    def to_stage(self):
-        return {
-            'basename': self.basename
-        }
-
-    @property
-    def basename(self):
-        return "{}.tar".format(self.name)
-
-    @property
-    def tar(self):
-        return tempfile_tar(self.pfiles)
-
-    @property
-    def tar_size(self):
-        spos = self.tar.tell()
-        self.tar.seek(0, os.SEEK_END)
-        size = self.tar.tell() - spos
-        self.tar.seek(spos)
-        return size
-
-    @property
-    def name(self):
-        if self._name is None:
-            self._name = self._get_project_name()
-        return self._name
-
-    def _get_project_name(self):
-        if os.path.isdir(self.project_path):
-            return os.path.basename(self.project_path)
-        else:
-            return os.path.splitext(os.path.basename(self.project_path))[0]
-
-
-class PFile(object):
-    def __init__(self, **kwargs):
-        self.fullpath = kwargs.get('fullpath', None)
-        self.basename = kwargs.get('basename', None)
-        self.relativepath = kwargs.get('relativepath', None)
-        self.size = kwargs.get('size', None)
-        self.populate()
-
-    def __str__(self):
-        if self.is_dir():
-            return self.relativepath
-        else:
-            return "[{}] {}".format(self.size, self.relativepath)
-
-    def __repr__(self):
-        return self.__str__()
-
-    def __eq__(self, other):
-        return self.fullpath == other.fullpath
-
-    def is_dir(self):
-        return os.path.isdir(self.fullpath)
-
-    def validate(self, validator):
-        if inspect.isfunction(validator):
-            return validator(basename=self.basename,
-                             relativepath=self.relativepath,
-                             fullpath=self.fullpath)
-        elif inspect.isclass(validator):
-            return validator(self)()
-        raise BinstarError("Invalid validator {}".format(validator))
-
-    def populate(self):
-        if self.size is None:
-            self.size = os.stat(self.fullpath).st_size
-        if self.basename is None:
-            self.basename = os.path.basename(self.fullpath)
-
-    def to_dict(self):
-        return {
-            'basename': self.basename,
-            'size': self.size,
-            'relativepath': self.relativepath
-        }
 
 
 def get_files(project_path, klass=None):
---
+++ binstar_client/utils/projects/test/test_models.py
@@ -0,0 +1,70 @@
+import unittest
+from binstar_client.utils.test.utils import example_path
+from ..models import CondaProject, PFile
+
+
+class CondaProjectTestCase(unittest.TestCase):
+    def test_initialize(self):
+        cp = CondaProject(example_path('bokeh-apps/weather'))
+        self.assertEqual(cp.metadata, {})
+
+        cp = CondaProject(example_path('bokeh-apps/weather'), summary="weather")
+        self.assertEqual(cp.metadata, {'summary': 'weather'})
+
+        cp = CondaProject(example_path('bokeh-apps/weather'), description="aa")
+        self.assertEqual(cp.metadata, {'description': 'aa'})
+
+    def test_to_stage(self):
+        cp = CondaProject(example_path('bokeh-apps/weather'))
+        self.assertEqual(cp.to_stage(), {'basename': 'weather.tar'})
+
+    def test_get_project_name_from_file(self):
+        prj = CondaProject(example_path('bokeh-apps/timeout.py'))
+        self.assertEqual(prj.name, 'timeout')
+
+    def test_get_project_name_from_dir(self):
+        prj = CondaProject(example_path('bokeh-apps/weather'))
+        self.assertEqual(prj.name, 'weather')
+
+    def test_ignore_empty_options(self):
+        prj = CondaProject(example_path('bokeh-apps/weather'), version='1')
+        self.assertEqual(prj.metadata['version'], '1')
+        self.assertNotIn('summary', prj.metadata)
+        self.assertNotIn('description', prj.metadata)
+
+
+class PFileTestCase(unittest.TestCase):
+    def test_size(self):
+        pfile = PFile(fullpath=example_path('bokeh-apps/timeout.py'))
+        self.assertEqual(pfile.size, 1523)
+
+    def test_basename(self):
+        pfile = PFile(fullpath=example_path('bokeh-apps/timeout.py'))
+        self.assertEqual(pfile.basename, 'timeout.py')
+
+    def test_str(self):
+        pfile = PFile(fullpath=example_path('bokeh-apps/timeout.py'),
+                      relativepath='bokeh-apps/timeout.py')
+        self.assertEqual(str(pfile), "[1523] bokeh-apps/timeout.py")
+
+    def test_validate_with_function(self):
+        def function(**kwargs):
+            return kwargs['basename'].endswith('.py')
+
+        pfile = PFile(fullpath=example_path('bokeh-apps/timeout.py'))
+        assert pfile.validate(function)
+
+    def test_validate_with_class(self):
+        class Validate(object):
+            def __init__(self, pfile):
+                pass
+
+            def __call__(self):
+                return True
+
+        pfile = PFile(fullpath=example_path('bokeh-apps/timeout.py'))
+        assert pfile.validate(Validate)
+
+
+if __name__ == '__main__':
+    unittest.main()
