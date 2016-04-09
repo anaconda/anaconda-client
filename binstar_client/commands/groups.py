89abbbcc5925f011274b85a7e97db631443ea590
Derek Ludwig <dludwig@continuum.io>
2016-04-08 17:15:24 -0700

    Clean up groups command some more

--- binstar_client/commands/groups.py
+++ binstar_client/commands/groups.py
@@ -18,12 +18,13 @@ def main(args):
     aserver_api = get_server_api(args.token, args.site, args.log_level)
     spec = args.spec
     action = args.action
+    verbose = args.log_level == logging.DEBUG
 
     if action == 'add':
-        result = aserver_api.add_group(spec.org, spec.group_name, args.perms)
-        pprint(result)
+        aserver_api.add_group(spec.org, spec.group_name, args.perms)
+        print('Created the group %s' % (spec,))
     elif action == 'show':
-        if spec.group_name:
+        if spec._group_name:
             result = aserver_api.group(spec.org, spec.group_name)
             pprint(result)
         else:
@@ -32,18 +33,22 @@ def main(args):
 
     elif action == 'members':
         result = aserver_api.group_members(spec.org, spec.group_name)
-        user_list(result, args.verbose)
+        user_list(result, verbose)
     elif action == 'add_member':
         aserver_api.add_group_member(spec.org, spec.group_name, spec.member)
+        print('Added the user "{spec.member}" to the group "{spec.org}/{spec.group_name}"'.format(spec=spec))
     elif action == 'remove_member':
         aserver_api.remove_group_member(spec.org, spec.group_name, spec.member)
+        print('Removed the user "{spec.member}" from the group "{spec.org}/{spec.group_name}"'.format(spec=spec))
     elif action == 'packages':
         result = aserver_api.group_packages(spec.org, spec.group_name)
-        package_list(result, args.verbose)
+        package_list(result, verbose)
     elif action == 'add_package':
         aserver_api.add_group_package(spec.org, spec.group_name, spec.member)
+        print('Added the package "{spec.member}" to the group "{spec.org}/{spec.group_name}"'.format(spec=spec))
     elif action == 'remove_package':
         aserver_api.remove_group_package(spec.org, spec.group_name, spec.member)
+        print('Removed the package "{spec.member}" from the group "{spec.org}/{spec.group_name}"'.format(spec=spec))
     else:
         raise NotImplementedError(args.action)
 
@@ -53,11 +58,14 @@ def add_parser(subparsers):
                                     help='Manage Groups',
                                     description=__doc__)
 
-    parser.add_argument('action', help='asdf',
-                        choices=['add', 'show', 'members', 'add_member', 'remove_member',
-                                 'packages', 'add_package', 'remove_package'], nargs='?')
-    parser.add_argument('spec', help=group_spec.__doc__, nargs='?', type=group_spec)
-    parser.add_argument('--perms', help='group permissions', choices=['read', 'write', 'admin'], default='read')
-
+    parser.add_argument('action',
+                        choices=['add', 'show', 'members', 'add_member',
+                                 'remove_member', 'packages', 'add_package',
+                                 'remove_package'],
+                        help='The group management command to execute')
+    parser.add_argument('spec', type=group_spec,
+                        help=group_spec.__doc__)
+    parser.add_argument('--perms', choices=['read', 'write', 'admin'], default='read',
+                        help='The permission the group should provide')
 
     parser.set_defaults(main=main)
