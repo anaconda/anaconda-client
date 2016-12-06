b7bddb41681b263095c6a31bc99ba1a8c152c21c
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2016-12-06 12:08:02 -0500

    Simplify api and test

--- binstar_client/tests/test_license.py
+++ binstar_client/tests/test_license.py
@@ -8,10 +8,8 @@ class Test(unittest.TestCase):
     @urlpatch
     def test_licenses_array_param(self, urls):
         api = Binstar()
-        urls.register(method='GET',
-                      path='/license/?product=produc_1&product=product_2',
-                      content='[]')
-        licenses = api.user_licenses(products=['product_1', 'product_2'])
+        urls.register(method='GET', path='/license', content='[]')
+        licenses = api.user_licenses()
         urls.assertAllCalled()
 
 
