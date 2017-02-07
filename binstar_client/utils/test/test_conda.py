c21e33bf6a1a8dd28b30eeeeb8c9aceed45c5957
j0hn <j0hn.com.ar@gmail.com>
2017-02-07 10:19:03 -0300

    fixed a couple of tests

--- binstar_client/utils/test/test_conda.py
+++ binstar_client/utils/test/test_conda.py
@@ -1,4 +1,5 @@
 import os
+import mock
 
 from binstar_client.utils import conda
 
@@ -9,22 +10,20 @@ def test_conda_root():
     assert get_conda_root() is not None
 
 
-def test_conda_root_outside_root_environment(monkeypatch):
-    called_mock = { 'called' : False }
-    def mock_import_conda_root():
-        called_mock['called'] = True
+@mock.patch('binstar_client.utils.conda._import_conda_root')
+def test_conda_root_outside_root_environment(mock_import_conda_root):
+    def _import_conda_root():
         raise ImportError("did not import it")
 
-    monkeypatch.setattr('binstar_client.utils.conda._import_conda_root', mock_import_conda_root)
-
+    mock_import_conda_root.side_effect = _import_conda_root
     from binstar_client.utils.conda import get_conda_root
 
     assert get_conda_root() is not None
 
-    assert called_mock['called']
+    assert mock_import_conda_root.called
 
 
-def test_conda_root_from_conda_info(monkeypatch):
+def test_conda_root_from_conda_info():
     from binstar_client.utils.conda import _conda_root_from_conda_info
 
     conda_root = _conda_root_from_conda_info()
