83b17dde92c9615758b5f55195f4be74a7ce4a52
Agustin Barto <abarto@anaconda.com>
2018-02-20 14:10:48 -0300

    Support yaml tags. Fix typing.

--- binstar_client/utils/yaml.py
+++ binstar_client/utils/yaml.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from yaml import safe_dump, safe_load
+from yaml import dump, load, safe_load
 
 
 def yaml_load(stream):
     """Loads a dictionary from a stream"""
-    return safe_load(stream)
+    return load(stream)
 
 
 def yaml_dump(data, stream=None):
     """Dumps an object to a YAML string"""
-    return safe_dump(data, stream=stream, default_flow_style=False)
+    return dump(data, stream=stream, default_flow_style=False)
