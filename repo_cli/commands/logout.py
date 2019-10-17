ab88704a3526b8af02b124d18fb0ffb8a8bf44f2
Fabio Pliger <fabio.pliger@gmail.com>
2019-10-17 10:35:56 -0500

    make sure logout cmd doesn't use old binstar code

--- repo_cli/commands/logout.py
+++ repo_cli/commands/logout.py
@@ -1,12 +1,12 @@
 """
 Log out from binstar
 """
-from binstar_client.utils import get_server_api, remove_token
+from ..utils .config import get_server_api, remove_token
 
 import logging
-from binstar_client import errors
+from .. import errors
 
-logger = logging.getLogger('binstar.logout')
+logger = logging.getLogger('repo_cli')
 
 
 def main(args):
@@ -15,11 +15,11 @@ def main(args):
     if aserver_api.token:
     # TODO: named 'application' because I was using the github model
     # Change it to name once we release the latest version of binstar server
-        try:
-            aserver_api.remove_authentication()
-        except errors.Unauthorized as err:
-            logger.debug("The token that you are trying to remove may not be valid"
-                      "{}".format(err))
+    #     try:
+        #     aserver_api.remove_authentication()
+        # except errors.Unauthorized as err:
+        #     logger.debug("The token that you are trying to remove may not be valid"
+        #               "{}".format(err))
 
         remove_token(args)
         logger.info("logout successful")
