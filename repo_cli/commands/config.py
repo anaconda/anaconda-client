7f58fabccf044cc2266eff2fed60c2eaf2ef296a
Fabio Pliger <fabio.pliger@gmail.com>
2019-12-03 12:27:07 -0600

    adapt config command to new standard cmd structure

--- repo_cli/commands/config.py
+++ repo_cli/commands/config.py
@@ -68,117 +68,128 @@ from argparse import RawDescriptionHelpFormatter
 
 from six import text_type
 
-from binstar_client.errors import ShowHelp
-from binstar_client.utils.config import (SEARCH_PATH, USER_CONFIG, SYSTEM_CONFIG, CONFIGURATION_KEYS,
-                                         get_config, save_config, load_config, load_file_configs)
+# from binstar_client.errors import ShowHelp
+# from binstar_client.utils.config import (SEARCH_PATH, USER_CONFIG, SYSTEM_CONFIG, CONFIGURATION_KEYS,
+#                                          get_config, save_config, load_config, load_file_configs)
+
+from .. import errors
+from ..utils.config import (SEARCH_PATH, USER_CONFIG, SYSTEM_CONFIG, CONFIGURATION_KEYS,
+                            get_config, save_config, load_config, load_file_configs)
 from ..utils.yaml import yaml_dump, safe_load
 
-logger = logging.getLogger('binstar.config')
-
-DEPRECATED = {
-    'verify_ssl': 'Please use ssl_verify instead'
-}
+from .base import SubCommandBase
 
 
-def recursive_set(config_data, key, value, type_):
-    while '.' in key:
-        prefix, key = key.split('.', 1)
-        config_data = config_data.setdefault(prefix, {})
-
-    if key not in CONFIGURATION_KEYS:
-        logger.warning('"%s" is not a known configuration key', key)
-
-    if key in DEPRECATED.keys():
-        message = "{} is deprecated: {}".format(key, DEPRECATED[key])
-        logger.warning(message)
-
-    config_data[key] = type_(value)
+# self.log = logging.getself.log('binstar.config')
+#
+DEPRECATED = {}
+#     'verify_ssl': 'Please use ssl_verify instead'
+# }
 
 
-def recursive_remove(config_data, key):
-    while '.' in key:
-        if not config_data:
+class SubCommand(SubCommandBase):
+    name = "config"
+
+    def main(self):
+
+        config = get_config()
+
+        if self.args.show:
+            self.log.info(yaml_dump(config))
             return
-        prefix, key = key.split('.', 1)
-        config_data = config_data.get(prefix, {})
 
-    del config_data[key]
+        if self.args.show_sources:
+            config_files = load_file_configs(SEARCH_PATH)
+            for path in config_files:
+                self.log.info('==> %s <==', path)
+                self.log.info(yaml_dump(config_files[path]))
+            return
 
+        if self.args.get:
+            if self.args.get in config:
+                self.log.info(config[self.args.get])
+            else:
+                self.log.info("The value of '%s' is not set." % self.args.get)
+            return
 
-def main(args):
-    config = get_config()
+        if self.args.files:
+            self.log.info('User Config: %s' % USER_CONFIG)
+            self.log.info('System Config: %s' % SYSTEM_CONFIG)
+            return
 
-    if args.show:
-        logger.info(yaml_dump(config))
-        return
+        config_file = USER_CONFIG if self.args.user else SYSTEM_CONFIG
 
-    if args.show_sources:
-        config_files = load_file_configs(SEARCH_PATH)
-        for path in config_files:
-            logger.info('==> %s <==', path)
-            logger.info(yaml_dump(config_files[path]))
-        return
+        config = load_config(config_file)
 
-    if args.get:
-        if args.get in config:
-            logger.info(config[args.get])
-        else:
-            logger.info("The value of '%s' is not set." % args.get)
-        return
+        for key, value in self.args.set:
+            self.recursive_set(config, key, value, self.args.type)
 
-    if args.files:
-        logger.info('User Config: %s' % USER_CONFIG)
-        logger.info('System Config: %s' % SYSTEM_CONFIG)
-        return
+        for key in self.args.remove:
+            try:
+                self.recursive_remove(config, key)
+            except KeyError:
+                self.log.error("Key %s does not exist" % key)
 
-    config_file = USER_CONFIG if args.user else SYSTEM_CONFIG
+        if not (self.args.set or self.args.remove):
+            raise errors.ShowHelp()
 
-    config = load_config(config_file)
-
-    for key, value in args.set:
-        recursive_set(config, key, value, args.type)
-
-    for key in args.remove:
-        try:
-            recursive_remove(config, key)
-        except KeyError:
-            logger.error("Key %s does not exist" % key)
-
-    if not (args.set or args.remove):
-        raise ShowHelp()
-
-    save_config(config, config_file)
+        save_config(config, config_file)
 
 
-def add_parser(subparsers):
-    description = 'Anaconda client configuration'
-    parser = subparsers.add_parser('config',
-                                   help=description,
-                                   description=description,
-                                   epilog=__doc__,
-                                   formatter_class=RawDescriptionHelpFormatter)
+    def recursive_remove(self, config_data, key):
+        while '.' in key:
+            if not config_data:
+                return
+            prefix, key = key.split('.', 1)
+            config_data = config_data.get(prefix, {})
 
-    parser.add_argument('--type', default=safe_load,
-                        help='The type of the values in the set commands')
+        del config_data[key]
 
-    agroup = parser.add_argument_group('actions')
 
-    agroup.add_argument('--set', nargs=2, action='append', default=[],
-                        help='sets a new variable: name value', metavar=('name', 'value'))
-    agroup.add_argument('--get', metavar='name',
-                        help='get value: name')
-    agroup.add_argument('--remove', action='append', default=[],
-                        help='removes a variable')
-    agroup.add_argument('--show', action='store_true', default=False,
-                        help='show all variables')
-    agroup.add_argument('-f', '--files', action='store_true',
-                        help='show the config file names')
-    agroup.add_argument('--show-sources', action='store_true',
-                        help='Display all identified config sources')
-    lgroup = parser.add_argument_group('location')
-    lgroup.add_argument('-u', '--user', action='store_true', dest='user', default=True,
-                        help='set a variable for this user')
-    lgroup.add_argument('-s', '--system', '--site', action='store_false', dest='user',
-                        help='set a variable for all users on this machine')
+    def recursive_set(self, config_data, key, value, type_):
+        while '.' in key:
+            prefix, key = key.split('.', 1)
+            config_data = config_data.setdefault(prefix, {})
 
-    parser.set_defaults(main=main, sub_parser=parser)
+        if key not in CONFIGURATION_KEYS:
+            self.log.warning('"%s" is not a known configuration key', key)
+
+        if key in DEPRECATED.keys():
+            message = "{} is deprecated: {}".format(key, DEPRECATED[key])
+            self.log.warning(message)
+
+        config_data[key] = type_(value)
+
+
+    def add_parser(self, subparsers):
+        description = 'Anaconda client configuration'
+        parser = subparsers.add_parser('config',
+                                       help=description,
+                                       description=description,
+                                       epilog=__doc__,
+                                       formatter_class=RawDescriptionHelpFormatter)
+
+        parser.add_argument('--type', default=safe_load,
+                            help='The type of the values in the set commands')
+
+        agroup = parser.add_argument_group('actions')
+
+        agroup.add_argument('--set', nargs=2, action='append', default=[],
+                            help='sets a new variable: name value', metavar=('name', 'value'))
+        agroup.add_argument('--get', metavar='name',
+                            help='get value: name')
+        agroup.add_argument('--remove', action='append', default=[],
+                            help='removes a variable')
+        agroup.add_argument('--show', action='store_true', default=False,
+                            help='show all variables')
+        agroup.add_argument('-f', '--files', action='store_true',
+                            help='show the config file names')
+        agroup.add_argument('--show-sources', action='store_true',
+                            help='Display all identified config sources')
+        lgroup = parser.add_argument_group('location')
+        lgroup.add_argument('-u', '--user', action='store_true', dest='user', default=True,
+                            help='set a variable for this user')
+        lgroup.add_argument('-s', '--system', '--site', action='store_false', dest='user',
+                            help='set a variable for all users on this machine')
+
+        parser.set_defaults(main=self.main, sub_parser=parser)
