c2a0406ddb3362d313d7549806d200679c79a4f9
Fabio Pliger <fabio.pliger@gmail.com>
2019-12-04 15:39:37 -0600

    add bulk commands base class

--- repo_cli/commands/base.py
+++ repo_cli/commands/base.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from os.path import dirname
 import logging
 import pkgutil
+from six.moves.urllib.parse import urlparse
 
 from ..utils.api import RepoApi
 from ..utils.config import store_token, get_config, load_token, DEFAULT_URL
@@ -78,7 +79,6 @@ class RepoCommand:
         _setup_logging(self.log, log_level=self.args.log_level, show_traceback=self.args.show_traceback,
                        disable_ssl_warnings=self.args.disable_ssl_warnings)
         self.config = config.get_config(site=self.args.site)
-
         self.url = self.config.get('url', DEFAULT_URL)
         self.api = RepoApi(base_url=self.url)
 
@@ -165,3 +165,158 @@ class SubCommandBase:
     @property
     def access_token(self):
         return self.parent._access_token
+
+
+class BulkActionCommand(SubCommandBase):
+    name = None
+
+    def main(self):
+
+        for spec in self.args.specs:
+            try:
+                if spec._filename:
+                    self.exec_bulk_action(spec.channel, self.args.family, spec.package, spec.version, spec.filename, spec)
+                elif spec._version:
+                    self.exec_bulk_action(spec.channel, self.args.family, spec.package, spec.version, spec=spec)
+                elif spec._package:
+                    self.exec_bulk_action(spec.channel, self.args.family, spec.package, spec=spec)
+                else:
+                    self.log.error('Invalid package specification: %s', spec)
+
+            except errors.NotFound:
+                if self.args.force:
+                    self.log.warning('', exc_info=True)
+                    continue
+                else:
+                    raise
+
+    def exec_bulk_action(self, channel, family, artifact, version=None, filename=None, spec=None):
+        base_item = {
+                "name": artifact,
+                "family": family,
+        }
+
+        items = []
+        if version or filename:
+            packages = self.api.get_channel_artifacts_files(channel, family, artifact, version, filename)
+
+            if not packages:
+                self.log.warning('No files matches were found for the provided spec: %s\n' % (spec))
+                return
+
+            files_descr = []
+            for filep in packages:
+                files_descr.append('PACKAGE: {name}:{version}-{ckey}; PLATFORM: {platform}; FILENAME: {fn}'.format(**filep))
+                item = dict(base_item)
+                item['ckey'] = filep['ckey']
+                items.append(item)
+
+            affected_files = '\n'.join(files_descr)
+            msg = 'Are you sure you want to {self.name} the package release %s ? The following ' \
+                  'will be affected: \n\n %s\n\nConfirm?' % (spec, affected_files)
+
+        else:
+            msg = 'Conform action %s on spec %s ? (and all data with it?)' % (self.name, spec)
+            items = [base_item]
+
+        if self.args.force or bool_input(msg, False):
+            self.api.channel_artifacts_bulk_actions(channel, self.name, items)
+            self.log.info('%s action successful\n' % self.name)
+        else:
+            self.log.info('%s action not executed\n' % self.name)
+
+    def add_parser(self, subparsers):
+        raise NotImplementedError
+
+
+
+
+class PackageSpec(object):
+    def __init__(self, channel, package=None, version=None, filename=None, attrs=None, spec_str=None):
+        self._user = channel
+        self._package = package
+        self._version = version
+        self._filename = filename
+        self.attrs = attrs
+        if spec_str:
+            self.spec_str = spec_str
+        else:
+            spec_str = str(channel)
+            if package:
+                spec_str = '%s/%s' % (spec_str, package)
+            if version:
+                spec_str = '%s/%s' % (spec_str, version)
+            if filename:
+                spec_str = '%s/%s' % (spec_str, filename)
+            self.spec_str = spec_str
+
+    @classmethod
+    def from_string(cls, spec_string):
+        channel = spec_string
+        package = version = filename = None
+        attrs = {}
+        if '::' in channel:
+            channel, package = channel.split('::', 1)
+        if package and '/' in package:
+            package, version = package.split('/', 1)
+
+        if version and '/' in version:
+            version, filename = version.split('/', 1)
+
+        if filename and '?' in filename:
+            filename, qsl = filename.rsplit('?', 1)
+            attrs = dict(urlparse.parse_qsl(qsl))
+
+        return PackageSpec(channel, package, version, filename, attrs, spec_string)
+
+    def __str__(self):
+        return self.spec_str
+
+    def __repr__(self):
+        return '<PackageSpec %r>' % (self.spec_str)
+
+    @property
+    def channel(self):
+        if self._user is None:
+            raise errors.UserError('user not given (got %r expected <username> )' % (self.spec_str,))
+        return self._user
+
+    @property
+    def name(self):
+        if self._package is None:
+            raise errors.UserError('package not given in spec (got %r expected <username>/<package> )' % (self.spec_str,))
+        return self._package
+
+    @property
+    def package(self):
+        if self._package is None:
+            raise errors.UserError('package not given in spec (got %r expected <username>/<package> )' % (self.spec_str,))
+        return self._package
+
+    @property
+    def version(self):
+        if self._version is None:
+            raise errors.UserError('version not given in spec (got %r expected <username>/<package>/<version> )' % (self.spec_str,))
+        return self._version
+
+    @property
+    def filename(self):
+        if self._filename is None:
+            raise errors.UserError('filename not given in spec (got %r expected <username>/<package>/<version>/<filename> )' % (self.spec_str,))
+        return self._filename
+
+
+def bool_input(prompt, default=True):
+    default_str = '[Y|n]' if default else '[y|N]'
+    while 1:
+        inpt = input('%s %s: ' % (prompt, default_str))
+        if inpt.lower() in ['y', 'yes'] and not default:
+            return True
+        elif inpt.lower() in ['', 'n', 'no'] and not default:
+            return False
+        elif inpt.lower() in ['', 'y', 'yes']:
+            return True
+        elif inpt.lower() in ['n', 'no']:
+            return False
+        else:
+            sys.stderr.write('please enter yes or no\n')
