8ab40b789d0a69894dcb95a0b53e6897c884ed2a
vshevchenkoanaconda <vshevchenko@anaconda.com>
2021-02-17 13:05:28 +0200

    AC-88: Add --update and --replace options for `copy` command

--- binstar_client/commands/copy.py
+++ binstar_client/commands/copy.py
@@ -2,8 +2,9 @@
 Copy packages from one account to another
 """
 from __future__ import unicode_literals, print_function
-from binstar_client.utils import get_server_api, parse_specs
 import logging
+
+from binstar_client.utils import get_server_api, parse_specs
 from binstar_client import errors
 
 logger = logging.getLogger('binstar.copy')
@@ -29,22 +30,23 @@ def main(args):
 
     files = aserver_api.copy(
         spec.user, spec.package, spec.version, spec._basename,
-        to_owner=args.to_owner, from_label=from_label, to_label=to_label
+        to_owner=args.to_owner, from_label=from_label, to_label=to_label, replace=args.replace, update=args.update
     )
 
     for binstar_file in files:
         print("Copied file: %(basename)s" % binstar_file)
+    update_msg = '\nNOTE: copy command with --update option doesn`t copy already existing files.' \
+                 ' Try to use --replace to overwrite existing data'
+    no_copied_files = "Did not copy any files. Please check your inputs with\n\n\tanaconda show {}".format(spec)
 
-    if files:
-        logger.info("Copied %i files" % len(files))
-    else:
-        logger.warning("Did not copy any files. Please check your inputs with\n\n\tanaconda show %s" % spec)
+    logger.info("Copied {} files!{}".format(len(files), update_msg if args.update else ''))
+
+    if not (files or args.update):
+        logger.warning(no_copied_files)
 
 
 def add_parser(subparsers):
-    parser = subparsers.add_parser('copy',
-                                      help='Copy packages from one account to another',
-                                      description=__doc__)
+    parser = subparsers.add_parser('copy', help='Copy packages from one account to another', description=__doc__)
 
     parser.add_argument('spec', help=('Package - written as user/package/version[/filename] '
                                       'If filename is not given, copy all files in the version'), type=parse_specs)
@@ -55,4 +57,10 @@ def add_parser(subparsers):
 
     _from.add_argument('--from-label', help='Label to copy packages from', default='main')
     _to.add_argument('--to-label', help='Label to put all packages into', default='main')
+
+    method_group = parser.add_mutually_exclusive_group()
+
+    method_group.add_argument('--replace', help='Overwrite destination package metadata', action='store_true')
+    method_group.add_argument('--update',
+                              help='Update missing data in destination package metadata', action='store_true')
     parser.set_defaults(main=main)
