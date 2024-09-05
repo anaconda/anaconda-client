504c665fdc038e947ec6d9aa077912757a473321
mattkram <mkramer@anaconda.com>
2024-09-05 12:42:19 -0500

    tidy: Clean up imports

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -22,7 +22,9 @@ from pytest import MonkeyPatch
 from typer import Typer
 from typer import rich_utils
 from typer.testing import CliRunner
+
 import anaconda_cli_base.cli
+
 import binstar_client.plugins
 import binstar_client.scripts.cli
 from binstar_client import commands
@@ -32,8 +34,7 @@ from binstar_client.plugins import (
     DEPRECATED_SUBCOMMANDS,
     SUBCOMMANDS_WITH_NEW_CLI,
 )
-from binstar_client.utils import parse_specs
-from binstar_client.utils.spec import group_spec
+from binstar_client.utils.spec import parse_specs, group_spec
 
 BASE_COMMANDS = {"login", "logout", "whoami"}
 HIDDEN_SUBCOMMANDS = ALL_SUBCOMMANDS - BASE_COMMANDS - NON_HIDDEN_SUBCOMMANDS
