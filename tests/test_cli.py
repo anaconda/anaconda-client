9d22d08c3e7a01e0013f87d504fe6bd6e0955716
mattkram <mkramer@anaconda.com>
2024-09-06 16:56:56 -0500

    Migrate show command and get parity

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -687,11 +687,6 @@ def test_arg_parsing_groups_command(cli_mocker, prefix_args, args, mods):
 
 
 @pytest.mark.parametrize(
-    "org_prefix",
-    [[], ["org"]],
-    ids=["bare", "org"],
-)
-@pytest.mark.parametrize(
     "prefix_args, args, mods",
     [
         pytest.param([], [], dict(), id="defaults"),
@@ -699,19 +694,13 @@ def test_arg_parsing_groups_command(cli_mocker, prefix_args, args, mods):
         pytest.param(["--site", "my-site.com"], [], dict(site="my-site.com"), id="site"),
     ]
 )
-def test_arg_parsing_show_command(monkeypatch, mocker, org_prefix, prefix_args, args, mods):
-
+def test_arg_parsing_show_command(cli_mocker, prefix_args, args, mods):
     spec = "someone/some-package/some-version"
-    args = prefix_args + org_prefix + ["show"] + args + [spec]
 
-    monkeypatch.setattr(sys, "argv", ["/path/to/anaconda"] + args)
-
-    runner = CliRunner()
-
-    mock = mocker.patch("binstar_client.commands.show.main")
-
-    result = runner.invoke(anaconda_cli_base.cli.app, args)
+    mock = cli_mocker(main_func="binstar_client.commands.show.main")
+    result = mock.invoke(["show", *args, spec], prefix_args=prefix_args)
     assert result.exit_code == 0, result.stdout
+    mock.assert_main_called_once()
 
     defaults = dict(
         token=None,
@@ -719,7 +708,7 @@ def test_arg_parsing_show_command(monkeypatch, mocker, org_prefix, prefix_args, 
         spec=parse_specs(spec),
     )
     expected = {**defaults, **mods}
-    mock.assert_called_once_with(args=Namespace(**expected))
+    mock.assert_main_args_contains(expected)
 
 
 @pytest.mark.parametrize(
