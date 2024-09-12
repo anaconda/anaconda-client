b4a96d6c8339800a147d5fbf6d9b599bff8ceaf4
mattkram <mkramer@anaconda.com>
2024-09-11 22:54:00 -0500

    Add test for login, and load credentials from environment variables/file

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,8 +2,12 @@ import os
 import subprocess
 
 import pytest
+from dotenv import load_dotenv
 
 from binstar_client.__about__ import __version__
+
+
+load_dotenv()
 
 
 def test_conda_installed():
@@ -19,8 +23,8 @@ def test_expect_installed():
 @pytest.fixture()
 def command():
     if not (test_cmd := os.getenv("TEST_ENV")):
-        test_cmd = "anaconda" #"python -W ignore -m binstar_client.scripts.cli"
-    print(f"Using '{test_cmd}' as an anaconda command.\\nYou may change it by providing TST_CMD=... environment variable.\\n")
+        test_cmd = "anaconda"
+        print(f"Using '{test_cmd}' as an anaconda command.\\nYou may change it by providing TST_CMD=... environment variable.\\n")
     return test_cmd
 
 
@@ -38,3 +42,37 @@ def test_config_files(command):
 def test_config_show(command):
     result = subprocess.run([command, "config", "--show"], capture_output=True, text=True)
     assert result.returncode == 0, "Showing of configuration files test failed."
+
+
+def test_login(command):
+    username = os.environ["TEST_USERNAME"]
+    password = os.environ["TEST_PASSWORD"]
+
+    program = subprocess.Popen(
+        [
+            command,
+            "login",
+            "--username",
+            username,
+            "--password",
+            password,
+        ],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    # Here, we confirm that it's okay that we already have a token
+    [out, err] = program.communicate(b"y")
+
+    assert program.returncode == 0
+
+    # Now that we're authenticated, we can run commands!
+    result = subprocess.run(
+        [command, "whoami"],
+        capture_output=True,
+        text=True,
+    )
+
+    assert result.returncode == 0
+    assert f"Username: {username}" in result.stderr
