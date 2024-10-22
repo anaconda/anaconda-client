910273cdf4bf8daa8b6770655397f62635890936
mattkram <mkramer@anaconda.com>
2024-10-21 21:52:53 -0500

    chore: lint

--- tests/utils/test_parse_specs.py
+++ tests/utils/test_parse_specs.py
@@ -1,4 +1,7 @@
+# pylint: disable=missing-module-docstring,missing-function-docstring
 import binstar_client.utils.spec
+
+
 def test_parse_specs():
     spec = binstar_client.utils.spec.parse_specs("bkreider/foo/1.2.3/blah-1.2.3.tar.bz2?x=1")
     assert spec.user == "bkreider"
