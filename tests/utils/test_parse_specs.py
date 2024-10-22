8fbf2a20a2346aad38f04679f616e93aa54fdbc1
mattkram <mkramer@anaconda.com>
2024-10-21 21:48:32 -0500

    Add test for parse_specs to attempt to reproduce

---
+++ tests/utils/test_parse_specs.py
@@ -0,0 +1,8 @@
+import binstar_client.utils.spec
+def test_parse_specs():
+    spec = binstar_client.utils.spec.parse_specs("bkreider/foo/1.2.3/blah-1.2.3.tar.bz2?x=1")
+    assert spec.user == "bkreider"
+    assert spec.name == "foo"
+    assert spec.version == "1.2.3"
+    assert spec.basename == "blah-1.2.3.tar.bz2"
+    assert spec.attrs == {"x": "1"}
