b8adae22cec88196f1d494fb392608d3950e0440
mattkram <mkramer@anaconda.com>
2024-10-21 22:05:51 -0500

    Change username

--- tests/utils/test_spec.py
+++ tests/utils/test_spec.py
@@ -4,8 +4,8 @@ import binstar_client.utils.spec
 
 def test_parse_specs():
     # Reproducing for https://github.com/anaconda/anaconda-client/issues/642
-    spec = binstar_client.utils.spec.parse_specs("bkreider/foo/1.2.3/blah-1.2.3.tar.bz2?x=1")
-    assert spec.user == "bkreider"
+    spec = binstar_client.utils.spec.parse_specs("someuser/foo/1.2.3/blah-1.2.3.tar.bz2?x=1")
+    assert spec.user == "someuser"
     assert spec.name == "foo"
     assert spec.version == "1.2.3"
     assert spec.basename == "blah-1.2.3.tar.bz2"
