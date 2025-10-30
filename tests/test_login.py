380475b0d3249fd6bd8b157e0e16216bc8a3e8de
Albert DeFusco <adefusco@anaconda.com>
2025-10-30 15:02:29 -0400

    test unified auth in cli subcommand

--- tests/test_login.py
+++ tests/test_login.py
@@ -9,15 +9,15 @@ from tests.fixture import CLITestCase, main
 from tests.urlmock import urlpatch
 
 
-@unittest.skip("migrated to unified anaconda-auth")
 class Test(CLITestCase):
     """Tests for authentication commands."""
 
     @unittest.mock.patch('binstar_client.commands.login.store_token')
     @unittest.mock.patch('getpass.getpass')
     @unittest.mock.patch('binstar_client.commands.login.input')
+    @unittest.mock.patch('binstar_client.commands.login.LEGACY_INTERACTIVE_LOGIN', True)
     @urlpatch
-    def test_login(self, urls, data, getpass, store_token):
+    def test_login_legacy(self, urls, data, getpass, store_token):
         data.return_value = 'test_user'
         getpass.return_value = 'password'
 
@@ -36,9 +36,39 @@ class Test(CLITestCase):
         self.assertTrue(store_token.called)
         self.assertEqual(store_token.call_args[0][0], 'a-token')
 
+    @unittest.mock.patch('binstar_client.commands.login._do_auth_flow')
+    @unittest.mock.patch('binstar_client.commands.login.store_token')
+    @urlpatch
+    def test_login_unified(self, urls, store_token, _do_auth_flow):
+        _do_auth_flow.return_value = "dot-com-access-token"
+
+        urls.register(path='/', method='HEAD', status=200)
+        well_known = urls.register(
+            path='/.well-known/openid-configuration',
+            method='GET',
+            status=200,
+            content='{"authorization_endpoint": "/auth", "token_endpoint": "/token"}'
+        )
+
+        auth = urls.register(method='POST', path='/authentications', content='{"token": "a-token"}')
+        main(['--show-traceback', 'login'])
+        self.assertIn('login successful', self.stream.getvalue())
+
+        well_known.assertCalled()
+        auth.assertCalled()
+
+        self.assertIn('Authorization', auth.req.headers)
+        self.assertIn('Bearer dot-com-access-token', auth.req.headers['Authorization'])
+
+        self.assertTrue(_do_auth_flow.called)
+
+        self.assertTrue(store_token.called)
+        self.assertEqual(store_token.call_args[0][0], 'a-token')
+
     @unittest.mock.patch('binstar_client.commands.login.store_token')
     @unittest.mock.patch('getpass.getpass')
     @unittest.mock.patch('binstar_client.commands.login.input')
+    @unittest.mock.patch('binstar_client.commands.login.LEGACY_INTERACTIVE_LOGIN', True)
     @urlpatch
     def test_login_compatible(self, urls, data, getpass, store_token):
         data.return_value = 'test_user'
