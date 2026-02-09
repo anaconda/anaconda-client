4391cff4a18bda8f58766c7b5e777ef42bd819d2
mattkram <mkramer@anaconda.com>
2026-02-08 21:39:33 -0600

    chore: Deprecate some imports

--- binstar_client/__init__.py
+++ binstar_client/__init__.py
@@ -13,5 +13,51 @@ from .utils.multipart_uploader import multipart_files_upload
 
 from binstar_client.client import NullAuth, HTTPBearerAuth
 from binstar_client.client import Binstar as Binstar
+from binstar_client.deprecations import DEPRECATE_IN_1_15_0, REMOVE_IN_2_0_0, deprecated
+
 
 logger = logging.getLogger('binstar')
+
+# Deprecated re-imports from binstar_client.mixins
+deprecated.constant(
+    deprecate_in=DEPRECATE_IN_1_15_0,
+    remove_in=REMOVE_IN_2_0_0,
+    constant="ChannelsMixin",
+    value=ChannelsMixin,
+    addendum="Use `binstar_client.mixins.channels.ChannelsMixin` instead",
+)
+
+deprecated.constant(
+    deprecate_in=DEPRECATE_IN_1_15_0,
+    remove_in=REMOVE_IN_2_0_0,
+    constant="OrgMixin",
+    value=OrgMixin,
+    addendum="Use `binstar_client.mixins.organizations.OrgMixin` instead",
+)
+
+deprecated.constant(
+    deprecate_in=DEPRECATE_IN_1_15_0,
+    remove_in=REMOVE_IN_2_0_0,
+    constant="PackageMixin",
+    value=PackageMixin,
+    addendum="Use `binstar_client.mixins.package.PackageMixin` instead",
+)
+
+# Deprecated re-imports from binstar_client.client
+deprecated.constant(
+    deprecate_in=DEPRECATE_IN_1_15_0,
+    remove_in=REMOVE_IN_2_0_0,
+    constant="NullAuth",
+    value=NullAuth,
+    addendum="Use `binstar_client.client.NullAuth` instead",
+)
+
+deprecated.constant(
+    deprecate_in=DEPRECATE_IN_1_15_0,
+    remove_in=REMOVE_IN_2_0_0,
+    constant="HTTPBearerAuth",
+    value=HTTPBearerAuth,
+    addendum="Use `binstar_client.client.HTTPBearerAuth` instead",
+)
+
+del DEPRECATE_IN_1_15_0, REMOVE_IN_2_0_0, deprecated
