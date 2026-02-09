5587edd24df5b687bf1852bbdc048544ebbc1cd1
mattkram <mkramer@anaconda.com>
2026-02-09 23:15:12 +0000

    chore: Deprecate __about__ module

--- binstar_client/__about__.py
+++ binstar_client/__about__.py
@@ -1,5 +1,12 @@
-"""Details about package."""
+from binstar_client import __version__
+from binstar_client.deprecations import DEPRECATE_IN_1_15_0, REMOVE_IN_2_0_0, deprecated
 
-from ._version import __version__
+deprecated.constant(
+    deprecate_in=DEPRECATE_IN_1_15_0,
+    remove_in=REMOVE_IN_2_0_0,
+    constant="__version__",
+    value=__version__,
+    addendum="Use `binstar_client.__version__` instead",
+)
 
-__all__ = ['__version__']
+del DEPRECATE_IN_1_15_0, REMOVE_IN_2_0_0, deprecated
