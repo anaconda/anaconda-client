10c739df8e2b0cad9afb2953d6203ab2e9e97714
mattkram <mkramer@anaconda.com>
2026-06-12 14:35:06 -0500

    refac: Remove unneeded get_config and use RepoApi class directly

--- binstar_client/repo/upload.py
+++ binstar_client/repo/upload.py
@@ -7,20 +7,15 @@ import logging
 import os
 
 from binstar_client import errors
-from binstar_client.repo.api import get_repo_api
-from binstar_client.utils.config import get_config
+from binstar_client.repo.api import RepoApi
 
 logger = logging.getLogger('binstar.repo')
 
 
 def upload_main(arguments: argparse.Namespace, channel: str) -> None:
     """Upload to a repo-core server instead of anaconda.org."""
-    config = get_config(site=arguments.site)
-    url = config.get('url', 'https://repo.anaconda.com/api')
     url = 'https://stage.anaconda.com/api/repo'
-    token = arguments.token
-
-    api = get_repo_api(url=url, token=token, verify_ssl=config.get('ssl_verify', True))
+    api = RepoApi(base_url=url, user_token=arguments.token)
 
     for filepath_list in arguments.files:
         for filepath in filepath_list:
