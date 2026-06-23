29c164efbbee8ce3c2e88451b6eb3587f1c48136
condathan <jnguyen@anaconda.com>
2026-06-23 18:26:40 +0200

    HUB-3348 conda repo channels integration (#874)
    * port conda repo channels and upload subcommands to here
    
    * don't print raw error status response content
    
    * chore(pre-commit): linting
    
    * incorporate PR feedback: rm upload endpoint, rm repo subcommand, move channels upward to be directly callable, rely on anaconda-auth for base url and client setup
    
    * chore(pre-commit): linting
    
    * fix lint error, propogate unlogged in error, try to fix action
    
    * chore(pre-commit): linting
    
    * fix tests
    
    * fix failing tests
    
    * refactor to remove config file (too sparse), move imports to top
    
    * required to move imports to prevent test failure
    
    * fix cli string parsing
    
    * correct repo path
    
    * fix tests
    
    * use gh action from main
    
    * fix display issue
    
    * print subchannels with channel ie namespace
    
    * use namespace-channels endpoint, abstract top-level channel to namespace, resolver for non-slash channels + namespace selection
    
    * chore(pre-commit): linting
    
    * fix lint error
    
    * rm detect, its not relevant to this PR
    
    * change namespace resolver to namespace and channel resolver to resolve ambiguities, allow no-namespaced channel creation
    
    * chore(pre-commit): linting
    
    * change channels subcommand to channel, lint fix
    
    * overload channel subcommand and add deprecation version for legacy options
    
    * chore(pre-commit): linting
    
    * fix: Add short -h help option
    
    * move repocore client to own file, pr feedback
    
    * pull all subchannels and use pager view if its too many to view in one screen
    
    * remove authenticated privacy setting
    
    ---------
    
    Co-authored-by: Anaconda Bot <devops+anaconda-bot@anaconda.com>
    Co-authored-by: mattkram <mkramer@anaconda.com>

---
+++ binstar_client/repocore/errors.py
@@ -0,0 +1,27 @@
+"""Repocore-specific exceptions."""
+
+
+class RepoCoreError(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not hasattr(self, "message"):
+            self.message = args[0] if args else None
+
+
+class Unauthorized(RepoCoreError):
+    def __init__(self, message=None):
+        self.msg = message or "The provided token does not allow you to perform this operation"
+        super().__init__(self.msg)
+
+
+class LoginRequiredError(RepoCoreError):
+    def __init__(self):
+        super().__init__("Authentication required. Please run 'anaconda login' and try again.")
+
+
+class InvalidName(RepoCoreError):
+    pass
+
+
+class NoDefaultChannel(RepoCoreError):
+    pass
